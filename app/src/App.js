e295a9908f446b10b75bc55912e18512e5df7e91
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2024-04-12 13:01:24 +0530

    12-04-2024

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Navbar/>
+      </div>
+      <Routes>
+        
+      </Routes>
+
+
+    </div>
+  );
+}
+
+export default App;
